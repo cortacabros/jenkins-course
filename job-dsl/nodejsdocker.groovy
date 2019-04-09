0925589e2ce689a0b894bd6813557465b4b64909
ko3yn <anton.kozorez@gmail.com>
2019-04-09 15:59:37 +0300

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('ko3yn/docker-jenkins_test')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('General')
+            registryCredentials('Jenkins')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
